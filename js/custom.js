dee854385ccb5b03c47d93361b8d150f6da66edc
jabedulalam <86180558+Jabedul-alam@users.noreply.github.com>
2021-09-28 18:10:12 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+$(function (){
+    $(window).on('scroll',function(){
+        //script for sticy menu;
+        var scroolpostion=$(window).scrollTop();
+        if(scroolpostion > 0){
+            $('#main-nav').addClass('stickynav');
+        }else{
+            $('#main-nav').removeClass('stickynav');
+        }
+    
+        //back to top
+        if(scroolpostion > 600){
+            $('#back').fadeIn(1000);
+        }else{
+            $('#back').fadeOut(1000);
+        }
+        
+        
+    })
+    $('#back').on('click',function(){
+        $("html,body").animate({
+            scrollTop:0
+        },1000)
+    
+    })
+})
